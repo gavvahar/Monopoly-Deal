5ac2614d372a717b5c0561623b84080b623a9c36
Nihar Gavva <23710337+gavvahar@users.noreply.github.com>
2025-08-29 14:36:40 -0400

    Update shell.nix to install dependencies after activating virtual environment

--- shell.nix
+++ shell.nix
@@ -15,6 +15,6 @@ pkgs.mkShell {
     if [ ! -d env ]; then
       python3 -m venv env
     fi
-    exec ${pkgs.fish}/bin/fish -C 'source ./env/bin/activate.fish'
+    exec ${pkgs.fish}/bin/fish -C 'source ./env/bin/activate.fish; pip install --upgrade pip; pip3 install -r ./requirements.txt'
   '';
 }
