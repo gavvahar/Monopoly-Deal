76f004ed8035c76cc4542f291603ec076af8d51c
gavvahar <23710337+gavvahar@users.noreply.github.com>
2025-08-29 13:56:29 -0400

    Add additional tools to development environment in shell.nix

--- shell.nix
+++ shell.nix
@@ -4,6 +4,10 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.python312
     pkgs.fish
+    pkgs.gh
+    pkgs.zoxide
+    pkgs.tmux
+    pkgs.tmate
   ];
   shellHook = ''
     exec ${pkgs.fish}/bin/fish
